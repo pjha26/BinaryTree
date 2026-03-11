a5a30b81001c77a8f47ef7ea0e01f0f7bb546f37
pjha26 <praj80624@gmail.com>
2026-03-12 03:52:51 +0530

    new file:   Solution.java

---
+++ Solution.java
@@ -0,0 +1,21 @@
+import javax.swing.tree.TreeNode;
+
+public class Solution {
+    int ans = 0;
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) return 0;
+        dfs(root, targetSum);           // paths starting at this node
+        pathSum(root.left, targetSum);  // start from left child
+        pathSum(root.right, targetSum); // start from right child
+        return ans;
+    }
+    private void dfs(TreeNode node, long target) {
+        if (node == null) return;
+        if (node.val == target) ans++;
+        dfs(node.left, target - node.val);
+        dfs(node.right, target - node.val);
+    }
+}
+    
+    
+
